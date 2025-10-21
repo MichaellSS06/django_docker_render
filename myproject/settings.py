f1ac8f9560fab1ec6dd380847edf8d0fea6243ec
MichaellLaptop <michaellhuanca@gmail.com>
2025-10-21 09:17:54 -0500

    Fix STATIC_ROOT for Koyeb3

--- myproject/settings.py
+++ myproject/settings.py
@@ -165,6 +165,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # -------------------------------
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_HSTS_SECONDS = 3600
